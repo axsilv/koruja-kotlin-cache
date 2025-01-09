be79fdc16750669a61d8d97e667101a9f34e3c74
Alex Silva <alexfabianojr@gmail.com>
2025-01-09 00:24:37 -0300

    fix gradle

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+    java
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20-Beta1"
+}
+
+group = "com.koruja.kotlin.cache"
+version = "0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+subprojects {
+    if (file("src/main/kotlin").isDirectory || file("src/main/resource").isDirectory) {
+
+        apply {
+            plugin("org.jetbrains.kotlin.jvm")
+            plugin("org.jetbrains.kotlin.plugin.serialization")
+        }
+
+        tasks.withType<Test> { useJUnitPlatform() }
+
+        repositories {
+            mavenCentral()
+        }
+
+        dependencies {
+            implementation(rootProject.libs.coroutines)
+            implementation(rootProject.libs.datetime)
+            implementation(rootProject.libs.serialization.core)
+            implementation(rootProject.libs.serialization.json)
+
+            testImplementation(rootProject.libs.kotest)
+            testImplementation(rootProject.libs.kotest.junit5)
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 rootProject.name = "koruja-kotlin-cache"
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 include(
     "core",
     "localfile",
